67ee8ba26604282a11a51117025e009c385690aa
Jimena Medina <medina.jime@gmail.com>
2023-03-29 18:26:37 -0300

    clases bq y dpl

--- function-bq-dlp-catalog/bq.py
+++ function-bq-dlp-catalog/bq.py
@@ -6,24 +6,38 @@ class BigQuerySchemaRows:
     dataset  = 'jimedina_pd'
     table = 'pdata'
     table_id = f'{project}.{dataset}.{table}'
-    limit = "100"
+    limit = 100
 
-    def __init__(self,project=None,dataset=None,table=None):
+    def __init__(self,project:str=None,
+                 dataset:str=None,table:str=None,limit:int=None):
         self.bq_client = bigquery.Client()
         if project and dataset and table: self.table_id = f'{project}.{dataset}.{table}'
+        if limit: self.limit = limit
 
 
-    def get_query(self):
+    def get_query(self) -> str:
+        """makes the sql string to query
+
+        Returns:
+            str: sql query
+        """
         query = "SELECT * "
-        #columns_string = ",".join(columns) if columns else "*"
-        #query += columns_string
         query += f" FROM `{self.table_id}` "
         limit_string = f"LIMIT {self.limit}" if self.limit else ""
         query += limit_string
         return query
 
-    def get_headers(self):
+    def get_headers(self) -> list:
+        """Obtaining a list with column labels
 
+        Returns:
+            list: list of column names
+            headers = [
+                { "name": "col1"},
+                {"name" : "col2"}
+
+            ]
+        """
         #Obtención de esquema json de la tabla BQ
         table_bq = self.bq_client.get_table(self.table_id)
         f = io.StringIO("")
@@ -31,33 +45,18 @@ class BigQuerySchemaRows:
         bq_schema_json = json.loads(f.getvalue())
     
         #Mapeo de datos para pasar a DLP el schema
-        
         headers = []
         for i in bq_schema_json:
             headers.append({ "name": i['name']})
-        
-        '''
-            headers = [
-                { "name": "col1"},
-                {"name" : "col2"}
-
-            ]
-        '''    
+           
         return headers
     
-    def get_rows(self):
-        #Consulta de datos desde BQ para ingresar a DLP
-        sqlQuery = self.get_query() 
-        query_job = self.bq_client.query(sqlQuery)
-        bqrows = query_job.result()
+    def get_rows(self) -> list:
+        """Obtaining table rows
 
-        rows = []
-        records = [dict(row) for row in bqrows]
-
-        for row in records:
-            rows.append({"values": [{"string_value": str(cell_val)} for cell_val in row.values()]}) 
-
-        '''
+        Returns:
+            list: Obtaining a list with the contents of rows
+            
         rows = [
         #row1  { "values": [
                             { "string_value":"row1.value1"},
@@ -69,19 +68,31 @@ class BigQuerySchemaRows:
             ]},
 
         ]
-        
-        '''
+        """
+        #Consulta de datos desde BQ para ingresar a DLP
+        sqlQuery = self.get_query() 
+        query_job = self.bq_client.query(sqlQuery)
+        bqrows = query_job.result()
+
+        rows = []
+        records = [dict(row) for row in bqrows]
+
+        for row in records:
+            rows.append({"values": [{"string_value": str(cell_val)} for cell_val in row.values()]}) 
+
         return rows
     
-    def get_item(self,headers,rows):
-        #Armado del objeto final con los headers y los rows
-        table_dlp = {}
-        table_dlp["headers"] = headers
-        table_dlp["rows"] = rows
-        item = {"table": table_dlp}
+    def get_item(self,headers: list,rows: list) -> dict:
+        """Obtaining the final dict with headers and rows
 
-        '''
-        item = {
+        Args:
+            headers (list): List with column labels
+            rows (list): List with the contents of rows
+
+        Returns:
+            dict: Dict with headers and rows
+            
+            item = {
                 "table": {
                     "headers":[
                         { "name": "col1"}
@@ -99,21 +110,21 @@ class BigQuerySchemaRows:
                     ]
                 }
         }
+        """
         
-        '''
+        table_dlp = {}
+        table_dlp["headers"] = headers
+        table_dlp["rows"] = rows
+        item = {"table": table_dlp}
         return item
     
-    def get_results_to_dlp(self):
+    def get_results_to_dlp(self) -> dict:
+        """_summary_
+
+        Returns:
+            dict: _description_
+        """
         headers = self.get_headers()
         rows = self.get_rows()
         item = self.get_item(headers,rows)
         return item
-
-
-
-        
-
-
-
-
-
--- function-bq-dlp-catalog/dlp.py
+++ function-bq-dlp-catalog/dlp.py
@@ -20,13 +20,41 @@ class DlpInspect:
     "min_likelihood": dlp_v2.Likelihood.POSSIBLE #Trae likelihood a partir de POSSIBLE
     }
 
-    def __init__(self,item):    
-        #Instancia DLP
+    def __init__(self,item:dict): 
+        """ Class instance initialization
+
+        Args:
+            item (dict): input schema for inspection in dlp
+            
+            item = {
+                "table": {
+                    "headers":[
+                        { "name": "col1"}
+                        {"name":"col2" }
+                    ],
+                    rows: [
+                        #row1  { "values": [
+                            { "string_value":"row1.value-col1"},
+                            {"string_value": "row1.value-col2"}    
+                                ]},
+                        #row2  { "values": [
+                            { "string_value":"row2.value-col1"},
+                            {"string_value": "row2.value-col2"}    
+                                ]},
+                    ]
+                }
+        }
+        """
+        
         self.dlp = dlp_v2.DlpServiceClient()
         self.item = item
 
-
-    def response_inspect(self):
+    def response_inspect(self) -> dict:
+        """Inspects the data entered
+        
+        Returns:
+            dict: _description_
+        """
         response = self.dlp.inspect_content(
         request={"parent": DlpInspect.PARENT,
                   "item": self.item,
@@ -35,34 +63,14 @@ class DlpInspect:
         print (json.dumps(self.item))
         return response
 
-    def get_finding_results(self,response: dict) -> dict: #hay que tipar todas las funciones, especificando tipos de datos de entrada y de retorno de cada función
-        """_summary_
+    def get_finding_results(self,response: dict) -> dict:
+        """According to the infotypes found, it groups them in a dict according to their likelihood weighting
 
         Args:
-            response (dict): _description_
+            response (dict): result of the inspection
 
         Returns:
-            retorna un diccionario re fachero.
-        """
-        finding_results = {}
-        if response.result.findings:
-            for finding in response.result.findings:
-                try:
-                    column = finding.location.content_locations[0].record_location.field_id.name
-                    if column in finding_results:
-                        aux_infotypes = finding_results[column]
-                        if finding.info_type.name in aux_infotypes:
-                            aux_infotypes[finding.info_type.name] = aux_infotypes[finding.info_type.name] + DlpInspect.VALUE_LIKELIHOOD[finding.likelihood.name]
-                        else:
-                            aux_infotypes[finding.info_type.name] = DlpInspect.VALUE_LIKELIHOOD[finding.likelihood.name]
-                    else:
-                        finding_results[column] = {}
-                        aux_infotypes = finding_results[column]
-                        aux_infotypes[finding.info_type.name] = DlpInspect.VALUE_LIKELIHOOD[finding.likelihood.name]
-                except AttributeError:
-                    pass
-        
-            '''
+            returns a dictionary with the infotypes found with their weighting
             finding_results = {
                 col1:{
                     infotype1:300,
@@ -73,39 +81,53 @@ class DlpInspect:
                     infotype3:2.5
                 }
             }
-            
-            '''
+        """
+        finding_results = {}
+        if response.result.findings:
+            for finding in response.result.findings:
+                try:
+                    column = finding.location.content_locations[0].record_location.field_id.name
+                    aux_infotypes = finding_results[column] if column in finding_results else {}
+                    value = aux_infotypes[finding.info_type.name] if aux_infotypes[finding.info_type.name] else 0
+                    aux_infotypes[finding.info_type.name] = value + DlpInspect.VALUE_LIKELIHOOD[finding.likelihood.name]
+                except AttributeError:
+                    pass
 
         pprint(finding_results)
         return finding_results
 
-    def get_top_findings(self,finding_results):
+    def get_top_findings(self,finding_results:dict) -> dict:
+        """Returns a dict containing the columns with their infotype of highest weighting
+
+        Args:
+            finding_results (dict): _description_
+
+        Returns:
+            dict: Returns a dict containing the columns with their infotype of highest weighting
+            top_findings = { nombre_columna:infotype2,nombre_columna2:infotype1 }
+        """
         top_findings = {}
         
-        #Primero itera sobre las keys
         if(finding_results):
             for column in finding_results:
                 max_infotype = None
                 max_count = 0
 
-            #Iteración sobre los objetos
                 for it, cnt in finding_results.get(column).items():
                     if max_infotype == None or cnt > max_count:
                         max_infotype = it
                         max_count = cnt
                 top_findings[column] = max_infotype
-            '''
-            top_finding = { nombre_columna:infotype2,nombre_columna2:infotype1 }
-            '''
             
             return top_findings
 
-    def get_results_to_catalog(self):
+    def get_results_to_catalog(self) -> dict:
+        """Runs through all methods to obtain top_findings
+
+        Returns:
+            dict: Returns a dict containing the columns with their infotype of highest weighting
+        """
         response = self.response_inspect()
         finding_results = self.get_finding_results(response)
         top_findings = self.get_top_findings(finding_results)
         return top_findings
-
-
-
-
