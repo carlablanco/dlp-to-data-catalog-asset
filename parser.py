4e0cf8e4efd174d14c44bcd1c9da6cdabb50fe56
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-03-29 15:01:54 -0300

    errores resolved

--- parser.py
+++ parser.py
@@ -2,9 +2,11 @@
 # or representation for any use or purpose. Your use of it is subject to your
 # agreement with Google.
 """Parses arguments before running DLP Scan."""
+
 import argparse
 from typing import List, Type
 import sys
+
 
 def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
     """Parses command line arguments."""
@@ -13,21 +15,21 @@ def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
         "--project",
         type=str,
         required=True,
-        help="The project ID of the Cloud project to be used.")
+        help="The Cloud project to be used.")
     parser.add_argument(
         "--dataset",
         type=str,
         required=True,
-        help="The dataset ID of the BigQuery dataset to be used.")
+        help="The BigQuery dataset to be used.")
     parser.add_argument(
         "--table",
         type=str,
-        help="The table of the bigquery dataset to be used.")
+        help="The BigQuery table to be used.")
     parser.add_argument(
-        "--parent",
+        "--parent_infotype",
         type=str,
         required=True,
-        help="The group that the infotype belongs to.")
+        help="The group that the Infotype belongs to.")
     parser.add_argument(
         "--location",
         type=str,
@@ -36,11 +38,19 @@ def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
 
     return parser.parse_args(argv[0:])    
 
-def run(proyecto: str, dataset: str, table: str, parent: str, location: str):
-    """Run the main function with the arguments parsed."""
-    main(proyecto, dataset, table, parent, location)
+
+def run(project: str, dataset: str, table: str, parent_infotype: str, location: str):
+    """Run the main function with the arguments parsed.
+    Args:
+        project: The Google Cloud project id to use. 
+        dataset: The BigQuery dataset to be used.
+        table: The BigQuery table to be used.
+        parent_infotype:The group that the Infotype belongs to.
+        location: The default location to be used.
+    """
     raise NotImplementedError
 
+
 if __name__ == "__main__":
-    arg = parse_arguments(sys.argv)
-    run(arg.project, arg.dataset,  arg.table, arg.parent, arg.location)
+    args = parse_arguments(sys.argv)
+    run(args.project, args.dataset,  args.table, args.parent_infotype, args.location)
