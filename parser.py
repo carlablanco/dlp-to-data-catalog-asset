339a35e0932763b843f7e6ef987e0e7feab26891
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-03-31 10:28:17 -0300

    changes

--- parser.py
+++ parser.py
@@ -17,6 +17,16 @@ def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
         required=True,
         help="The Google Cloud project to be used.")
     parser.add_argument(
+        "--language_code",
+        type=str,
+        required=True,
+        help="The BCP-47 language code to use, e.g. 'en-US'.")
+    parser.add_argument(
+        "--location",
+        type=str,
+        required=True,
+        help=" Location where the jobs will be run.")
+    parser.add_argument(
         "--dataset",
         type=str,
         required=True,
@@ -26,32 +36,22 @@ def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
         type=str,
         help="""The BigQuery table to be scanned. Optional.
                 If None, the entire dataset will be scanned.""")
-    parser.add_argument(
-        "--language_code",
-        type=str,
-        required=True,
-        help="The BCP-47 language code to use, e.g. 'en-US'.")
-    parser.add_argument(
-        "--location",
-        type=str,
-        required=True,
-        help="The default location to be used.")
     return parser.parse_args(argv[0:])
 
 
-def run(project: str, dataset: str, language_code: str, location: str, table: str = None):
+def run(project: str, language_code: str, location: str, dataset: str, table: str = None):
     """Runs DLP inspection scan and tags the results to Data Catalog.
     Args:
         project: Project ID for which the client acts on behalf of.
+        language_code: The BCP-47 language code to use, e.g. 'en-US'.
+        location: Location where the jobs will be run.
         dataset: The BigQuery dataset to be scanned.
         table: The BigQuery table to be scanned. Optional.
                 If None, the entire dataset will be scanned.
-        language_code: The BCP-47 language code to use, e.g. 'en-US'.
-        location: Location where the jobs will be run.
     """
     raise NotImplementedError
 
 
 if __name__ == "__main__":
     args = parse_arguments(sys.argv)
-    run(args.project, args.dataset, args.language_code, args.location, args.table)
+    run(args.project, args.language_code, args.location, args.dataset, args.table)
