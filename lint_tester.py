a5da7d57e9c0ff5c13dd10ba62a1aa1ced414ce3
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-04-03 13:59:18 -0300

    teste linting checks

--- lint_tester.py
+++ lint_tester.py
@@ -1,1 +1,4 @@
-print('El linter funciona joya si tira el error del espacio al final ')   
+print('El linter funciona joya si no tira el error en esta linea')
+
+
+print("el lint funciona si tira error en esta linea porque esta va a ser una linea copn mushisimos caracteres")
