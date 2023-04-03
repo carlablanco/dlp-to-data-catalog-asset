b31bdd610f47f9e927f40afc08ae05860627296b
Jimena Medina <medina.jime@gmail.com>
2023-04-03 10:21:04 -0300

    correcciones docstrings y metodos

--- function-bq-dlp-catalog/preprocess_bq.py
+++ function-bq-dlp-catalog/preprocess_bq.py
@@ -9,82 +9,150 @@ import io
 from google.cloud import bigquery
 
 
-class BigQuerySchemaRows:
-    """A class used to retrieve BigQuery schema rows and convert them to DLP table items.
+class PreprocessBqToDlp:
+    """Class for preprocessing BigQuery tables into Data Loss Prevention tables.
     """
+
     def __init__(self, project: str,
                  dataset: str, table: Optional[str] = None):
+        """
+        Args:
+            project (str): The name of the GCP project where the BigQuery dataset exists.
+            dataset (str): The name of the BigQuery dataset.
+            table (str, optional): The name of the BigQuery table. Defaults to None.
+        """
         self.bq_client = bigquery.Client()
         self.project = project
         self.dataset = dataset
         self.table = table
 
     def get_query(self, table_id: str) -> str:
-        """Makes the sql string to query.
+        """Creates an SQL query as string.
 
         Args:
-            table_id (str): The fully qualified name of the BQ table.
+            table_id (str): The fully qualified name of the BigQuery table.
 
         Returns:
-            str: Sql query
+            str: SQL query as string.
         """
         query = f"SELECT *  FROM `{table_id}`"
         return query
 
-    def convert_to_dlp_table(self, table_id: str) -> dict:
-        """Converts a BQ table into a table item that can be ingested by DLP.
+    def get_bigquery_table(self, table_id: str) -> tuple:
+        """Retrieves the schema and content of a BigQuery table.
 
         Args:
-            table_id (str): The fully qualified name of the BQ table.
+            table_id (str): The fully qualified name of the BigQuery table.
 
         Returns:
-            dict: A table item that can be ingested by DLP.
+            tuple: A tuple containing the BigQuery schema and content.
         """
-        # Obtención de esquema json de la tabla BQ
         table_bq = self.bq_client.get_table(table_id)
-        f_var = io.StringIO("")
-        self.bq_client.schema_to_json(table_bq.schema, f_var)
-        bq_schema_json = json.loads(f_var.getvalue())
+        f_temp = io.StringIO("")
+        self.bq_client.schema_to_json(table_bq.schema, f_temp)
+        bq_schema = json.loads(f_temp.getvalue())
 
-        # Mapeo de datos del header
-        headers = []
-        for i in bq_schema_json:
-            headers.append({"name": i['name']})
-
-        # Mapeo de los rows
-        rows = []
         sql_query = self.get_query(table_id)
         query_job = self.bq_client.query(sql_query)
-        bqrows = query_job.result()
-        records = [dict(row) for row in bqrows]
-        for row in records:
+        query_results = query_job.result()
+
+        bq_rows_content = [dict(row) for row in query_results]
+
+        return bq_schema, bq_rows_content
+
+    def convert_to_dlp_table(self, bq_schema: dict, bq_rows_content: dict) -> dict:
+        """Converts a BigQuery table into an object that can be inspected by Data Loss Prevention.
+
+        Args:
+            bq_schema (dict): The schema of a BigQuery table.
+            bq_rows_content (dict): The content of a BigQuery table.
+
+        Returns:
+            dict: A table object that can be inspected by Data Loss Prevention.
+        """
+
+        headers = []
+        for i in bq_schema:
+            headers.append({"name": i['name']})
+
+        rows = []
+        for row in bq_rows_content:
             rows.append(
                 {"values": [{"string_value": str(cell_val)} for cell_val in row.values()]})
 
-        table_dlp = {}
-        table_dlp["headers"] = headers
-        table_dlp["rows"] = rows
-        item = {"table": table_dlp}
-        return item
+        table_dlp = {"table": {"headers": headers, "rows": rows}}
+
+        return table_dlp
 
     def get_dlp_table_list(self) -> list:
-        """Constructs a list of table items that can be ingested by DLP.
+        """Constructs a list of table objects that can be inspected by Data Loss Prevention.
 
         Returns:
-            list: A list of table items that can be ingested by DLP.
+            list: A list of table object that can be inspected by Data Loss Prevention.
+             
+            table_dlp_list = [
+                {
+                "table": {
+                    "headers": [
+                        {
+                            "name": "id"
+                        },
+                        {
+                            "name": "name"
+                        },
+                        {
+                            "name": "age"
+                        }
+                    ],
+                    "rows": [
+                        {
+                            "values": [
+                                {
+                                    "string_value": "1"
+                                },
+                                {
+                                    "string_value": "John"
+                                },
+                                {
+                                    "string_value": "25"
+                                }
+                            ]
+                        },
+                        {
+                            "values": [
+                                {
+                                    "string_value": "2"
+                                },
+                                {
+                                    "string_value": "Mary"
+                                },
+                                {
+                                    "string_value": "30"
+                                }
+                            ]
+                        },
+                    ]
+                }
+            }
+            ]
+            
         """
-        items = []
+
+        table_dlp_list = []
         if self.table:
             table_id = f'{self.project}.{self.dataset}.{self.table}'
-            item = self.convert_to_dlp_table(table_id)
-            items.append(item)
+            bq_schema, bq_rows_content = self.get_bigquery_table(table_id)
+            table_dlp = self.convert_to_dlp_table(bq_schema, bq_rows_content)
+            table_dlp_list.append(table_dlp)
         else:
             tables = list(self.bq_client.list_tables(self.dataset))
             if tables:
                 for table in tables:
                     table_id = f'{self.project}.{self.dataset}.{table.table_id}'
-                    item = self.convert_to_dlp_table(table_id)
-                    items.append(item)
+                    bq_schema, bq_rows_content = self.get_bigquery_table(
+                        table_id)
+                    table_dlp = self.convert_to_dlp_table(
+                        bq_schema, bq_rows_content)
+                    table_dlp_list.append(table_dlp)
 
-        return items
-   
+        return table_dlp_list
