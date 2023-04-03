e49ceb0a064d780c7aa28198c8db61e0692361cb
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-04-03 14:00:31 -0300

    should pass the lint

--- lint_tester.py
+++ lint_tester.py
@@ -1,4 +1,1 @@
 print('El linter funciona joya si no tira el error en esta linea')
-
-
-print("el lint funciona si tira error en esta linea porque esta va a ser una linea copn mushisimos caracteres")
