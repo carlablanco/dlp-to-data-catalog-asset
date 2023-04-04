e16c8e5e2d54aee13dfa62d3629892c181c8005d
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-04-04 10:11:53 -0300

    Delete lint_tester.py

--- lint_tester.py
+++
@@ -1,3 +0,0 @@
-'''This is the docstring needed to check if the python linter actually works
- correctly on lines that are longer than expected'''
-print('El linter funciona joya si no tira el error en  esta line')
