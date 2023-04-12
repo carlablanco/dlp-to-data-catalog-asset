44336cfeafcc421b3cebda847fd34c2d70591399
Jimena Medina <medina.jime@gmail.com>
2023-04-12 15:19:14 -0300

    correcciones run.py

--- dlp/run.py
+++ dlp/run.py
@@ -4,11 +4,11 @@
 """Parses arguments before running DLP Scan."""
 
 import argparse
-from typing import List, Type
-import sys
+from typing import Type
+from dlp.preprocess import Preprocessing
 
 
-def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
+def parse_arguments() -> Type[argparse.Namespace]:
     """Parses command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -36,7 +36,7 @@ def parse_arguments(argv: List[str]) -> Type[argparse.Namespace]:
         type=str,
         help="""The BigQuery table to be scanned. Optional.
                 If None, the entire dataset will be scanned.""")
-    return parser.parse_args(argv[0:])
+    return parser.parse_args()
 
 
 def run(project: str, language_code: str, location: str, dataset: str, table: str = None):
@@ -44,14 +44,18 @@ def run(project: str, language_code: str, location: str, dataset: str, table: st
     Args:
         project: Project ID for which the client acts on behalf of.
         language_code: The BCP-47 language code to use, e.g. 'en-US'.
-        location: Location where the jobs will be run.
+        param location: Location where the jobs will be run. 
         dataset: The BigQuery dataset to be scanned.
         table: The BigQuery table to be scanned. Optional.
                 If None, the entire dataset will be scanned.
     """
-    raise NotImplementedError
+
+    preprocess = Preprocessing(
+        project=project, dataset=dataset, table=table)
+    preprocess.get_dlp_table_list()
 
 
 if __name__ == "__main__":
-    args = parse_arguments(sys.argv)
-    run(args.project, args.language_code, args.location, args.dataset, args.table)
+    args = parse_arguments()
+    run(args.project, args.language_code,
+        args.location, args.dataset, args.table)
