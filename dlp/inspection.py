2a26c6e2f6d0ab859ba89c148332f28cdd98df2e
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-04-17 14:58:18 -0300

    changes

--- dlp/inspection.py
+++ dlp/inspection.py
@@ -3,28 +3,44 @@
 # agreement with Google.
 """Runs the DLP inspection over the preprocessed table."""
 
-from typing import Dict
+from typing import Dict, Optional
 from google.cloud import dlp_v2
 
 class DlpInspection:
     """The DlpInspection class performs a DLP inspection on a preprocessed
       table to identify sensitive information."""
-    def __init__(self, project_id: str, language_code: str, table: Dict):
+    def __init__(self, project_id: str, language_code: str, dicts_list: Optional[Dict]):
         """Initializes the class with the required data.
 
         Args:
             project_id: The project ID to be used.
             language_code: The BCP-47 language code to use, e.g. 'en-US'.
-            table: The table to be inspected in the correct format.
+            lista: The table to be inspected in the correct format.
         """
         self.dlp_client = dlp_v2.DlpServiceClient()
         self.project_id = project_id
         self.language_code = language_code
-        self.table = table
+        self.dicts_list = dicts_list
 
-    def get_table_inspected(self):
+    def main(self):
+        """Iterates over every dictionary, analyzes each one.
+            
+            Returns:
+                results: A list of dictionaries with the infotype with the
+                    highest likelihood."""
+        results = []
+        dicts_list = self.dicts_list
+        for table in dicts_list:
+            result = self.get_max_infotype(table)
+            results.append(result)
+        return results
+
+    def get_table_inspected(self, table: Dict):
         """Makes a call to the DLP API and analyzes the table acording to the
             language code that is provided by the user.
+
+            Args:
+                table: The table to be inspected in the correct format.
 
             Returns:
             The response from the API call.
@@ -38,11 +54,11 @@ class DlpInspection:
         }
         parent = f"projects/{self.project_id}"
         response = self.dlp_client.inspect_content(
-            request={"parent": parent, "item": self.table, "inspect_config": inspect_config})
+            request={"parent": parent, "item": table, "inspect_config": inspect_config})
         return response
 
-    def analyze_inspection_result(self) -> Dict:
-        """ Get the results of the inspection.
+    def analyze_inspection_result(self, table: Dict) -> Dict:
+        """ Processes the results of the inspection.
 
             This code iterates through an API response and constructs a
             dictionary.
@@ -51,13 +67,12 @@ class DlpInspection:
             In each sub-dictionary, the variable name is used as the key
             and the associated value is the likelihood.
 
-            infotype: The type of information the API contains.
-
-
+            Args:
+                table: The table to be inspected in the correct format.
 
             Returns:
-            finding_results: Por cada variable hay tiene su respectivo
-                infotype y liklihod value.
+                finding_results: For every variable there is a dictionary with
+                    the infotype and the likelihood value.
             Example: {'name': {'PERSON_NAME': 4.4}, 'age': {'AGE': 5.8}}
         """
         value_likelihood = {
@@ -66,7 +81,7 @@ class DlpInspection:
             "VERY_LIKELY":1.4
         }
         finding_results = {}
-        response = self.get_table_inspected()
+        response = self.get_table_inspected(table)
         if response.result.findings:
             for finding in response.result.findings:
                 try:
@@ -76,31 +91,43 @@ class DlpInspection:
 
                     infotypes = finding_results.setdefault(column, {})
                     likelihood = value_likelihood.get(finding.likelihood.name, 0)
-
+                    """If the infotype is already in the dictionary, sum
+                        likelihood value to the exisiting one."""
                     if finding.info_type.name in infotypes:
                         infotypes[finding.info_type.name] += likelihood
                     else:
+                        """If the infotype is not in the dictionary, add it with
+                            the likelihood value."""
                         infotypes[finding.info_type.name] = likelihood
                 except AttributeError:
                     pass
         return finding_results
 
-    def get_max_infotype(self) -> Dict:
+    def get_max_infotype(self, table: Dict) -> Dict:
         """Returns the infotype with the highest likelihood.
 
             Iterates over the finding results and returns the infotype with
             the highest likelihood.
 
+            Args:
+                table: The table to be inspected in the correct format.
+
             Returns:
             finding_results: A dictionary where each variable has its respective
-              "infotype" and "likelihood value"
+              "infotype" and "likelihood value."
         """
         top_findings = {}
-        finding_results = self.analyze_inspection_result()
+        finding_results = self.analyze_inspection_result(table)
+        
         for column in finding_results:
             max_infotype = None
             max_count = 0
             for infotype, count in finding_results.get(column).items():
+                """Add the infotype to the top_findings dict.
+                    If the infotype is already in the dict, sum the likelihood
+                    value to the existing one, otherwise add it with the
+                    likelihood value."""
+                
                 if max_infotype is None or count > max_count:
                     max_infotype = infotype
                     max_count = count
