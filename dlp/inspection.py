d2b716b8fbec75db2225fad978acca990bd00399
Agustin Alfie <69637974+alfie-agustin@users.noreply.github.com>
2023-04-18 11:51:50 -0300

    changes

--- dlp/inspection.py
+++ dlp/inspection.py
@@ -22,8 +22,8 @@ class DlpInspection:
         self.language_code = language_code
         self.tables = tables
 
-    def get_table_inspected(self):
-        """Get the table to be inspected with API call.
+    def get_inspection_parameters(self):
+        """Gets the table to be inspected with an API call.
 
             Args:
                 table: The particular table to be inspected in the correct format.
@@ -32,7 +32,8 @@ class DlpInspection:
                 parent: The project route in GCP.
                 inspect_config: The configuration for the inspection.
         """
-        info_types = self.dlp_client.list_info_types(request={"language_code": self.language_code})
+        info_types = self.dlp_client.list_info_types(
+                        request={"language_code": self.language_code})
         info_types_names = [info_type.name for info_type in info_types.info_types if
                             (self.language_code in info_type.name)]
         inspect_config = {
@@ -43,7 +44,7 @@ class DlpInspection:
         return parent, inspect_config
 
     def analyze_inspection_result(self, table_inspected: Dict) -> Dict:
-        """ Processes the results of the inspection.
+        """Processes the results of the inspection.
 
             This code iterates through an API response and constructs a
             dictionary.
@@ -70,25 +71,23 @@ class DlpInspection:
             for finding in table_inspected.result.findings:
                 try:
                     column = finding.location.content_locations[0].record_location.field_id.name
-                    if not column:
-                        raise Exception("No findings returned from API call.")
 
                     infotypes = finding_results.setdefault(column, {})
                     likelihood = value_likelihood.get(finding.likelihood.name, 0)
-                    #If the infotype is already in the dictionary, sum
-                    #likelihood value to the exisiting one.
+                    # If the infotype is already in the dictionary, sum
+                    # likelihood value to the exisiting one.
                     if finding.info_type.name in infotypes:
                         infotypes[finding.info_type.name] += likelihood
                     else:
-                        #If the infotype is not in the dictionary, add it with
-                        #the likelihood value.
+                        # If the infotype is not in the dictionary, add it with
+                        # the likelihood value.
                         infotypes[finding.info_type.name] = likelihood
                 except AttributeError:
-                    pass
+                    raise ValueError("AttributeError: No findings returned from API call.")
         return finding_results
 
     def get_max_infotype(self, finding_results: Dict) -> Dict:
-        """Returns the infotype with the highest likelihood.
+        """Gets the max infotype for each variable.
 
             Iterates over the finding results and returns the infotype with
             the highest likelihood.
@@ -105,10 +104,10 @@ class DlpInspection:
             max_infotype = None
             max_count = 0
             for infotype, count in finding_results.get(column).items():
-                #Add the infotype to the top_findings dict.
-                #If the infotype is already in the dict, sum the likelihood
-                #value to the existing one, otherwise add it with the
-                #likelihood value.
+                # Add the infotype to the top_findings dict.
+                # If the infotype is already in the dict, sum the likelihood
+                # value to the existing one, otherwise add it with the
+                # likelihood value.
                 if max_infotype is None or count > max_count:
                     max_infotype = infotype
                     max_count = count
@@ -122,13 +121,17 @@ class DlpInspection:
                 results: A list of dictionaries with the infotype with the
                     highest likelihood.
                     Example: [{'name': 'PERSON_NAME', 'age': 'AGE'},
-                     {'name2': 'PERSON_NAME', 'age2': 'AGE'}]"""
+                     {'DNI': 'GOVERMENT_ID', 'token': 'AUTH_TOKEN'}]"""
         results = []
-        parent, inspect_config = self.get_table_inspected()
+        parent, inspect_config = self.get_inspection_parameters()
         for table in self.tables:
+            # Get table inspeceted
             response = self.dlp_client.inspect_content(
                 request={"parent": parent, "item": table, "inspect_config": inspect_config})
+            # Processes the results of the inspection.
             finding_results = self.analyze_inspection_result(response)
+            # Gets the max infotype for each variable.
             top_findings = self.get_max_infotype(finding_results)
+            # Append to the results list.
             results.append(top_findings)
         return results
