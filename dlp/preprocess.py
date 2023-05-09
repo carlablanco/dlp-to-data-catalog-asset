903290b6578495b99c660fd460793918fb236d61
agustin <aalfie@itba.edu.ar>
2023-05-09 10:18:37 -0300

    commit

--- dlp/preprocess.py
+++ dlp/preprocess.py
@@ -3,6 +3,8 @@
 # agreement with Google.
 """Processes input data to fit to DLP inspection standards."""
 
+import pandas as pd
+import itertools
 from typing import List, Tuple, Dict
 from google.api_core.exceptions import NotFound
 from google.cloud import bigquery, dlp_v2
@@ -48,6 +50,7 @@ class Preprocessing:
          """
         content = []
         fields = table_id.schema
+
         rows_iter = self.bq_client.list_rows(table_id)
 
         if not rows_iter.total_rows:
@@ -59,9 +62,129 @@ class Preprocessing:
                 for i, field in enumerate(fields):
                     row_dict[field.name] = row[i]
                 content.append(row_dict)
+
         return content
 
-    def get_bigquery_data(self, table_id: str) -> Tuple[List[Dict], List[Dict]]:
+    def generateSchema(self, table_id):
+        """Generates a schema for a given table ID.
+
+            Args:
+                table_id (str): The ID of the table for which the schema needs to be generated.
+
+            Returns:
+                tuple: A tuple containing three lists - schema, nested_schema, and main_nested_schema.
+                    - schema (list): The list of fields in the schema.
+                    - nested_schema (list): The list of nested fields in the schema.
+                    - main_nested_schema (list): The list of main fields associated with the nested fields.
+
+            """
+        schema = []
+        nested_schema = []
+        main_nested_schema = []
+        fields = table_id.schema
+        for field in fields:
+            
+            record, nested, main_field = self.generateField(field)
+            if nested == True:
+                main_nested_schema.append(main_field)
+                nested_schema.append(record)
+            else:
+                schema.append(record)
+
+        return schema, nested_schema, main_nested_schema
+
+    def generateField(self, field):
+        """Generates a field for the given field object.
+
+        Args:
+            field: The field object for which the field needs to be generated.
+
+        Returns:
+            tuple: A tuple containing three values - record, nested, and
+            main_cell.
+                - record: The generated field or list of nested fields.
+                - nested (bool): Indicates if the field is nested or not.
+                - main_cell: The main field associated with the nested fields.
+
+        """
+        nested = False
+        main_cell = False 
+        if field.field_type == "RECORD":
+            recordField = []
+            for subfield in field.fields:
+                main_cell = field.name
+                cell = field.name+"."+subfield.name
+                if subfield.field_type == "RECORD":
+                    recordField.append(self.generateField(subfield)) 
+                else:
+                    recordField.append(cell)
+            nested = True
+            return recordField, nested, main_cell
+        else:
+            return field.name, nested, main_cell
+
+    def get_query(self, bq_schema, main_nested_schema, table_id):
+        """Creates an SQL query as string.
+
+        Args:
+           bq_schema (List): The schema of a BigQuery table.
+            main_nested_schema(List) : The columns with record type.
+            table_id (str): The fully qualified name of the BigQuery table.
+            
+        Returns:
+            str: SQL query as string. 
+        
+        """
+        #este no funciona para nestede del tipo array, hay q hacer varios if q vaya x cada caso
+        var = ', '.join(str(item) for item in bq_schema)
+
+        unnest = f"""UNNEST ([{main_nested_schema[0]}]) as 
+                    {main_nested_schema[0]}"""
+        query = f"""SELECT {var} 
+        FROM `{table_id}`, 
+        {unnest}""" 
+        return query
+
+    def get_rows_query(self, bq_schema, main_nested_schema, table_id ):
+        """ Retrives the content of the table.
+        
+        Args:
+            bq_schema (List): The schema of a BigQuery table.
+            main_nested_schema(List) : The columns with record type.
+            table_id (str): The fully qualified name of the BigQuery table.
+       
+       Returns:
+            List[Dict]: The content of the BigQuery table.
+            
+        
+        """
+        sql_query = self.get_query(bq_schema,main_nested_schema, table_id)
+        query_job = self.bq_client.query(sql_query)
+        query_results = query_job.result()
+
+        bq_rows_content = [dict(row) for row in query_results]
+
+        return bq_rows_content
+    
+    def get_data_types(self, table_id):
+        """ Gets the data types of the selected table.
+        
+        Args:
+            table_id (str): The fully qualified name of the BigQuery table.
+
+        Returns:
+            List: A complete list with the data types of the columns.
+        """
+        dtypes = []
+        
+        fields = table_id.schema
+        for field in fields:
+            dtypes.append(field.field_type)
+
+        return dtypes
+
+    def get_bigquery_data(self, table_id: str) -> Tuple[List[Dict], 
+                                                        List[Dict]]:
         """Retrieves the schema and content of a BigQuery table.
 
         Args:
@@ -76,16 +199,56 @@ class Preprocessing:
         except NotFound as exc:
             raise ValueError(f"Error retrieving table {table_id}.") from exc
 
-        table_schema = table_bq.schema
-        bq_schema = [schema_field.to_api_repr()
-                     for schema_field in table_schema]
+        dtypes = self.get_data_types(table_bq)
 
-        bq_rows_content = self.fetch_rows(table_bq)
+        if "RECORD" in dtypes:
+            table_schema, nested_schema, main_nested_schema = (
+                self.generateSchema(table_bq))
+            table_schema = self.flatten_list(table_schema)
+            nested_schema = self.flatten_list(nested_schema) 
+            bq_schema = table_schema + nested_schema
+
+            bq_rows_content = self.get_rows_query(bq_schema,
+                                                  main_nested_schema, table_bq)
+
+        else:
+
+            table_schema = table_bq.schema
+            bq_schema = [schema_field.to_api_repr()
+                         for schema_field in table_schema]
+            bq_rows_content = self.fetch_rows(table_bq)
 
         return bq_schema, bq_rows_content
+    
+    def flatten_list(self, lst):
+        """
+        Recursively flattens a nested list and returns a flattened list.
+
+        Args:
+            lst (list): The input list that needs to be flattened.
+
+        Returns:
+            list: The flattened list.
+
+        """
+        # Create an empty list to store the flattened elements.
+        flattened = []
+
+        # Iterate through each element in the list.
+        for element in lst:
+            # If the element is a list, recursively flatten the list.
+            if isinstance(element, list):
+                flattened += self.flatten_list(element)
+            else:
+                # If the element is not a list, add it to the flattened list.
+                flattened.append(element)
+
+        # Return the flattened list.
+        return flattened
 
     def convert_to_dlp_table(self, bq_schema: List[Dict],
-                             bq_content: List[Dict]) -> dlp_v2.Table:
+                             bq_content: List[Dict], table_id: str) ->(
+                             dlp_v2.Table):
         """Converts a BigQuery table into a DLP table.
 
         Converts a BigQuery table into a Data Loss Prevention table,
@@ -94,27 +257,47 @@ class Preprocessing:
         Args:
             bq_schema (List[Dict]): The schema of a BigQuery table.
             bq_content (List[Dict]): The content of a BigQuery table.
+            table_id (str): The fully qualified name of the BigQuery table.
 
         Returns:
             A table object that can be inspected by Data Loss Prevention.
         """
+        table_bq = self.bq_client.get_table(table_id)
+        dtypes = self.get_data_types(table_bq)
+        
         table_dlp = dlp_v2.Table()
-        table_dlp.headers = [
-            {"name": schema_object['name']} for schema_object in bq_schema
-        ]
+        
+        if "RECORD" in dtypes:
+            
+            table_dlp.headers = [
+                {"name": name} for name in bq_schema
+            ]
+            rows = []
+            for row in bq_content:
+                rows.append(dlp_v2.Table.Row(
+                    values=[dlp_v2.Value(
+                        string_value=str(cell_val)) for cell_val
+                        in row.values()]))
+            table_dlp.rows = rows
+            
+        else:
+            table_dlp.headers = [
+                {"name": schema_object['name']} for schema_object in bq_schema
+            ] 
 
-        rows = []
-        for row in bq_content:
-            rows.append(dlp_v2.Table.Row(
-                values=[dlp_v2.Value(
-                    string_value=str(cell_val)) for cell_val in row.values()]))
+            rows = []
+            for row in bq_content:
+                rows.append(dlp_v2.Table.Row(
+                    values=[dlp_v2.Value(
+                        string_value=str(cell_val)) for cell_val in
+                        row.values()]))
 
-        table_dlp.rows = rows
-
+            table_dlp.rows = rows
+        print(table_dlp.headers)
         return table_dlp
 
     def get_dlp_table_list(self) -> List[dlp_v2.Table]:
-        """Constructs a list of DLP Table objects
+        """Constructs a list of DLP Table objects.
 
         Returns:
             A list of Data Loss Prevention table objects.
@@ -128,9 +311,12 @@ class Preprocessing:
 
         if bigquery_tables:
             for table_name in bigquery_tables:
+                table_id = f'{self.project}.{self.dataset}.{table_name}'
                 schema, content = self.get_bigquery_data(
-                    f'{self.project}.{self.dataset}.{table_name}')
-                table_dlp = self.convert_to_dlp_table(schema, content)
+                    table_id)
+                
+                table_dlp = self.convert_to_dlp_table(schema, content,
+                                                      table_id)
                 dlp_tables_list.append(table_dlp)
-
+        
         return dlp_tables_list
